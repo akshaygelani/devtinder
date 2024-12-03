5ed05077ef3b9d4c904032fc64d9c377815590e9
akshaygelani <akshaygelani786@gmail.com>
2024-12-03 19:03:33 +0530

    feat: added initial server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = process.env.PORT;
+const app = express();
+
+app.use((req, res) => {
+  res.send('Hello from the server');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT: ${PORT}`);
+});
