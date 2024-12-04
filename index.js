1c74cb675bb3d57943f2546c98f0c7cfa2d36aca
akshaygelani <akshaygelani786@gmail.com>
2024-12-04 21:29:51 +0530

    feat: connect to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const connectDB = require('./src/config/database');
 const { PORT } = require('./src/utils/constants');
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server Started on PORT: ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    console.log('MongoDB Database Connected!!');
+    app.listen(PORT, () => {
+      console.log(`Server Started on PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log('Oops, There is some error in connecting to Database!');
+  });
