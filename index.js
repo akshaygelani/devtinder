0c1a76cb0b880c09c92b6d97d55414bcb3bbf40a
akshaygelani <akshaygelani786@gmail.com>
2024-12-04 21:10:58 +0530

    feat: remove as not needed

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
-const { authenticate } = require('./middlewares/authentication');
-const { isAdmin } = require('./middlewares/authorization');
 const PORT = process.env.PORT;
 const app = express();
-
-app.use(authenticate);
-app.use('/admin', isAdmin);
-app.get('/user', (req, res) => {
-  res.send('This is user data');
-});
-app.get('/admin/getUserInfo', (req, res) => {
-  res.send({ firstName: 'Akshay', lastName: 'Gelani' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server Started on PORT: ${PORT}`);
