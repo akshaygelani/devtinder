ff244551e1cc79eae521dc5f06fa205eaed8fcba
akshaygelani <akshaygelani786@gmail.com>
2024-12-07 18:28:51 +0530

    feat: corrected signup handler

--- src/handlers/auth/signup.js
+++ src/handlers/auth/signup.js
@@ -3,9 +3,9 @@ import { userModel } from '../../models/user.js';
 export const signUpHandler = async (req, res) => {
   const user = new userModel(req.body);
   try {
+    await user.save();
   } catch (error) {
     console.log('Oops Error:', error.message);
   }
-  await user.save();
   res.send('user saved successfully');
 };
