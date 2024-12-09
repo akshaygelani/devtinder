c492c3623f613446f5d41727cba99366c5d3e3f7
akshaygelani <akshaygelani786@gmail.com>
2024-12-09 22:11:37 +0530

    feat: improvement to signup handler. hashed the password

--- src/handlers/auth/signup.js
+++ src/handlers/auth/signup.js
@@ -1,8 +1,26 @@
 import { userModel } from '../../models/user.js';
+import bcrypt from 'bcrypt';
 
 export const signUpHandler = async (req, res) => {
   try {
-    const user = new userModel(req.body);
+    const { firstName, lastName, email, password } = req.body;
+
+    // schema validations
+    const existingUser = await userModel.findOne({ email: email });
+
+    if (existingUser) {
+      return res.status(400).send('Invalid body!');
+    }
+
+    // hash the password with auto generated salt
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new userModel({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+
     await user.save();
     res.status(200).send('Signup Successful!');
   } catch (error) {
