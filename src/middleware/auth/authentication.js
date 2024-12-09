13439ea2ae0751fdebf5a14c6d5a5b3f3593f12f
akshaygelani <akshaygelani786@gmail.com>
2024-12-10 00:27:59 +0530

    feat: verify jwt token in authentication middleware

--- src/middleware/auth/authentication.js
+++ src/middleware/auth/authentication.js
@@ -1,7 +1,25 @@
-export const verifyAuthentication = (req, res, next) => {
+import jwt from 'jsonwebtoken';
+import { userModel } from '../../models/user.js';
+
+export const verifyAuthentication = async (req, res, next) => {
   try {
+    // decode token
     let { access_token } = req.cookies;
-    console.log(access_token);
+    let payload = jwt.verify(access_token, 'asb@344');
+    if (!payload) {
+      return res.status(400).send('Invalid Token');
+    }
+
+    // find user and verify id
+    let { id } = payload;
+    let user = await userModel.findById(id);
+    if (!user) {
+      return res.status(400).send('Unauthorized!');
+    } else {
+      req.user = user;
+    }
+
+    // continue executing other middlewares
     next();
   } catch (error) {
     console.log('Oops Error:', error.message);
