b2aa3d630745d7cccf36d2638df7dd0bca8e22ae
akshaygelani <akshaygelani786@gmail.com>
2024-12-11 02:46:00 +0530

    feat: use common error functions

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,22 +1,25 @@
 import jwt from 'jsonwebtoken';
 import { userModel } from '../models/user.js';
+import { badRequest, unauthorized } from '../utils/responses.js';
 
 export const verifyAuthentication = async (req, res, next) => {
   // decode token
   let { access_token } = req.cookies;
+  if (!access_token) {
+    return badRequest(res, 'Token Missing');
+  }
   let payload = jwt.verify(access_token, 'asb@344');
   if (!payload) {
-    return res.status(400).send('Invalid Token');
+    return badRequest('Invalid Token');
   }
 
   // find user and verify id
   let { id } = payload;
   let user = await userModel.findById(id);
   if (!user) {
-    return res.status(400).send('Unauthorized!');
-  } else {
-    req.loggedInUser = user;
+    return unauthorized(res);
   }
+  req.loggedInUser = user;
 
   // continue executing other middlewares
   next();
