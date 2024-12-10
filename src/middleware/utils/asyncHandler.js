ab1f462904602163f4b471f0c94307c1b5842799
akshaygelani <akshaygelani786@gmail.com>
2024-12-11 01:33:21 +0530

    feat: updated variables

--- src/middleware/utils/asyncHandler.js
+++ src/middleware/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // async handler which catches runtime errors from all handlers and forwards flow to centralized error handler
-export const asyncHandler = (fn) => {
+export const asyncHandler = (handler) => {
   return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => {
+    Promise.resolve(handler(req, res, next)).catch((err) => {
       next(err);
     });
   };
