6fba4d0ecf843d90d3de7f774c9e9a8ecf629df6
akshaygelani <akshaygelani786@gmail.com>
2024-12-11 01:12:38 +0530

    feat: added IS_PROD to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,4 @@ config();
 export const env = process.env;
 export const PORT = env.PORT;
 export const MONGODB_CONNECTION_STRING = env.CONNECTION_STRING + env.DATABASE;
+export const IS_PROD = process.env.NODE_ENV === 'production';
