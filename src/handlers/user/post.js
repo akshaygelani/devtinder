0f2847ac9778611d7ab72a5249e2b2b8dbd58095
akshaygelani <akshaygelani786@gmail.com>
2024-12-11 12:25:53 +0530

    feat: add todo

--- src/handlers/user/post.js
+++ src/handlers/user/post.js
@@ -1,6 +1,7 @@
 import { userModel } from '../../models/user.js';
 
 export const postUserHandler = async (req, res) => {
+  // TODO: sanitize data, add validations and hash password
   const user = new userModel(req.body);
   await user.save();
   res.status(200).send('User added Successfully');
