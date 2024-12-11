48952da2a76eaa6556920b75f2336e89c8ab14ab
akshaygelani <akshaygelani786@gmail.com>
2024-12-11 13:35:58 +0530

    feat: send connection request handler

--- src/handlers/request/sendRequest.js
+++ src/handlers/request/sendRequest.js
@@ -1,3 +1,62 @@
-export const sendRequestHandler = (req, res) => {
-  // extract data
+import mongoose from 'mongoose';
+import { requestModel } from '../../models/request.js';
+import { userModel } from '../../models/user.js';
+import { success, badRequest } from '../../utils/responses.js';
+
+export const sendRequestHandler = async (req, res) => {
+  let { action } = req.body;
+  let allowedAction = ['ignored', 'interested'];
+  if (!allowedAction.includes(action)) {
+    return badRequest(res, 'Invalid Action', [
+      {
+        field: 'action',
+        message: "Supported actions are 'ignored' and 'interested'",
+      },
+    ]);
+  }
+
+  let loggedInUser = req.loggedInUser;
+
+  let fromUserId = loggedInUser.id;
+  let toUserId = req.params.toUserId;
+
+  if (!mongoose.isValidObjectId(toUserId)) {
+    return badRequest(res, 'Invalid toUserId');
+  }
+
+  if (fromUserId === toUserId) {
+    return badRequest(res, 'Cannot send connection request to self');
+  }
+
+  let toUser = await userModel.findById(toUserId);
+  if (!toUser) {
+    return badRequest(res, 'Incorrect toUserId');
+  }
+
+  let connectionRequests = await requestModel.findOne({
+    $or: [
+      {
+        fromUserId,
+        toUserId,
+      },
+      {
+        fromUserId: toUserId,
+        toUserId: fromUserId,
+      },
+    ],
+  });
+
+  if (connectionRequests) {
+    return badRequest(res, 'Connection request already exists!');
+  }
+
+  let request = new requestModel({
+    fromUserId,
+    toUserId,
+    status: action,
+  });
+
+  await request.save();
+
+  return success(res);
 };
