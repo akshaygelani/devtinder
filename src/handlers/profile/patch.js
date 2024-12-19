153b904876313a858afaf88f013db87877c62a27
akshaygelani <akshaygelani786@gmail.com>
2024-12-19 13:50:17 +0530

    feat: updated patch profile handler

--- src/handlers/profile/patch.js
+++ src/handlers/profile/patch.js
@@ -1,20 +1,12 @@
-export const patchProfileHandler = async (req, res) => {
-  let allowedUpdates = [
-    'firstName',
-    'lastName',
-    'age',
-    'gender',
-    'photoUrl',
-    'about',
-    'skills',
-  ];
+import { success, badRequest } from '../../utils/responses';
 
-  let isUpdateAllowed = Object.keys(req.body).every((field) =>
-    allowedUpdates.includes(field)
-  );
+export const patchProfileHandler = async (req, res) => {
+  let allowedUpdates = ['firstName', 'lastName', 'age', 'gender', 'photoUrl', 'about', 'skills'];
+
+  let isUpdateAllowed = Object.keys(req.body).every((field) => allowedUpdates.includes(field));
 
   if (!isUpdateAllowed) {
-    return res.status(400).send('Invalid Body');
+    return badRequest(res, 'Invalid Body!');
   }
   let loggedInUser = req.loggedInUser;
 
@@ -23,5 +15,5 @@ export const patchProfileHandler = async (req, res) => {
   });
 
   await loggedInUser.save();
-  res.json({ message: 'Record Updated!', data: loggedInUser });
+  return success(res, 'Record Updated!', loggedInUser);
 };
