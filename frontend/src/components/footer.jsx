24c6287841a70d751c91e5908060984162aa0585
akshaygelani <akshaygelani786@gmail.com>
2024-12-19 18:36:42 +0530

    feat: modified footer component

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className='footer footer-center bg-base-200 text-base-content p-4'>
+    <footer className='footer footer-center bg-base-200 text-base-content p-4 fixed bottom-0'>
       <aside>
         <p>Copyright © {new Date().getFullYear()} - All right reserved by DevTinder Pvt. Ltd</p>
       </aside>
