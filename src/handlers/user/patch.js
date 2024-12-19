9983ee8483c0fb2afd07492001462147d4a3e226
akshaygelani <akshaygelani786@gmail.com>
2024-12-19 13:44:18 +0530

    feat: updated patch user handler

--- src/handlers/user/patch.js
+++ src/handlers/user/patch.js
@@ -1,26 +1,18 @@
 import { userModel } from '../../models/user.js';
+import { accepted, badRequest } from '../../utils/responses.js';
 
 export const patchUserHandler = async (req, res) => {
   const { userId } = req.params;
   const body = req.body;
-  const allowedUpdate = [
-    'age',
-    'gender',
-    'password',
-    'about',
-    'photoUrl',
-    'skills',
-  ];
+  const allowedUpdate = ['age', 'gender', 'password', 'about', 'photoUrl', 'skills'];
 
-  let isUpdateAllowed = Object.keys(body).every((k) =>
-    allowedUpdate.includes(k)
-  );
+  let isUpdateAllowed = Object.keys(body).every((k) => allowedUpdate.includes(k));
   if (!isUpdateAllowed) {
-    return res.status(400).send('Invalid Body');
+    return badRequest(res, 'Invalid Body!');
   }
 
   await userModel.findByIdAndUpdate(userId, body, {
     runValidators: true,
   });
-  return res.status(202).send('Record updated successfully!');
+  return accepted('Record updated successfully!');
 };
