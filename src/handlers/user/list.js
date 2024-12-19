e8d33bf5344e1abac78630418423de14264dc75d
akshaygelani <akshaygelani786@gmail.com>
2024-12-19 13:47:06 +0530

    feat: updated list user handler

--- src/handlers/user/list.js
+++ src/handlers/user/list.js
@@ -1,10 +1,11 @@
 import { userModel } from '../../models/user.js';
+import { success, notFound } from '../../utils/responses.js';
 
 export const listUserHandler = async (req, res) => {
   const users = await userModel.find({});
   if (users.length == 0) {
-    res.status(404).send('No records found');
+    return notFound(res, 'No records found');
   } else {
-    res.status(200).send(users);
+    return success(res, 'Records fetch successfully!', users);
   }
 };
