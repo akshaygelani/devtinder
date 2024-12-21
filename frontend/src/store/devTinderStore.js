d242418af8203b72066b93b94785e298ecd1ff4d
akshaygelani <akshaygelani786@gmail.com>
2024-12-22 00:16:40 +0530

    feat: add requests slice

--- frontend/src/store/devTinderStore.js
+++ frontend/src/store/devTinderStore.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/user';
 import feedReducer from './slices/feed';
 import connectionsReducer from './slices/connections';
+import requestsReducer from './slices/requests';
 
 export const devTinderStore = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
     connections: connectionsReducer,
+    requests: requestsReducer,
   },
 });
