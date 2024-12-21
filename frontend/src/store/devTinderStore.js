b011576364faa17933b28c749668403f4f5701f4
akshaygelani <akshaygelani786@gmail.com>
2024-12-21 23:25:45 +0530

    feat: add connections slice

--- frontend/src/store/devTinderStore.js
+++ frontend/src/store/devTinderStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/user';
 import feedReducer from './slices/feed';
+import connectionsReducer from './slices/connections';
 
 export const devTinderStore = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
+    connections: connectionsReducer,
   },
 });
