81ec8c0c31a58a232b7993d37ea102e31885a8cf
akshaygelani <akshaygelani786@gmail.com>
2024-12-21 21:37:59 +0530

    feat: improved app ui

--- frontend/src/components/body.jsx
+++ frontend/src/components/body.jsx
@@ -46,7 +46,7 @@ function Body() {
   }, [navigate]);
 
   return (
-    <div className='font-akshay'>
+    <div className='font-akshay md:max-w-xl flex items-center flex-col m-auto h-screen'>
       <NavBar />
       <Outlet />
       <Footer />
--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className='footer footer-center bg-base-300 text-base-content p-4 fixed bottom-0'>
+    <footer className='bg-base-300 text-center p-4 mt-auto w-full'>
       <aside>
         <p>Copyright © {new Date().getFullYear()} - All right reserved by DevTinder Pvt. Ltd</p>
       </aside>
