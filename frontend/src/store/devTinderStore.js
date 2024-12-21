257828dbd998509dba458335bf51dfa45937967d
akshaygelani <akshaygelani786@gmail.com>
2024-12-21 18:13:21 +0530

    feat: add user feed slice

--- frontend/src/store/devTinderStore.js
+++ frontend/src/store/devTinderStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/user';
+import feedReducer from './slices/feed';
 
 export const devTinderStore = configureStore({
   reducer: {
     user: userReducer,
+    feed: feedReducer,
   },
 });
