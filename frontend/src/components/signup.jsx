3ec957119e4a27809d874ed1b9456db2b2c6c849
akshaygelani <akshaygelani786@gmail.com>
2024-12-21 16:51:07 +0530

    feat: added error and success toast

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -8,18 +8,30 @@ function SignUp() {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [showSuccessToast, setSuccessToast] = useState(false);
+  const [showErrorToast, setErrorToast] = useState(false);
 
   const navigate = useNavigate();
 
   const signUpHandler = async () => {
+    setError('');
     try {
-      const result = await axios.post(
+      await axios.post(
         API_BASE_URL + '/auth/signup',
         { firstName, lastName, email, password },
         { withCredentials: true }
       );
-      navigate('/signin');
+      setSuccessToast(true);
+      setTimeout(() => {
+        setSuccessToast(false);
+      }, 3000);
     } catch (error) {
+      setErrorToast(true);
+      setError(error.response?.data?.message);
+      setTimeout(() => {
+        setErrorToast(false);
+      }, 3000);
       console.error(error);
     }
   };
@@ -28,69 +40,85 @@ function SignUp() {
     navigate('/signin');
   };
   return (
-    <div className='flex justify-center my-3'>
-      <div className='card  bg-base-300 w-96 shadow-xl'>
-        <div className='card-body'>
-          <h2 className='card-title justify-center'>Create account</h2>
-          <label className='form-control w-full max-w-xs'>
-            <div className='label'>
-              <span className='label-text'>First Name</span>
+    <>
+      <div className='flex justify-center my-3'>
+        <div className='card  bg-base-300 w-96 shadow-xl'>
+          <div className='card-body'>
+            <h2 className='card-title justify-center'>Create account</h2>
+            <label className='form-control w-full max-w-xs'>
+              <div className='label'>
+                <span className='label-text'>First Name</span>
+              </div>
+              <input
+                type='text'
+                placeholder='Enter First Name'
+                value={firstName}
+                className='input input-bordered w-full max-w-xs'
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </label>
+            <label className='form-control w-full max-w-xs'>
+              <div className='label'>
+                <span className='label-text'>Last Name</span>
+              </div>
+              <input
+                type='text'
+                placeholder='Enter Last Name'
+                value={lastName}
+                className='input input-bordered w-full max-w-xs'
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </label>
+            <label className='form-control w-full max-w-xs'>
+              <div className='label'>
+                <span className='label-text'>Email</span>
+              </div>
+              <input
+                type='text'
+                placeholder='Enter Email'
+                value={email}
+                className='input input-bordered w-full max-w-xs'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label className='form-control w-full max-w-xs'>
+              <div className='label'>
+                <span className='label-text'>Password</span>
+              </div>
+              <input
+                type='password'
+                placeholder='Enter Password'
+                value={password}
+                className='input input-bordered w-full max-w-xs'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            {error && <p className='text-red-500'>Error: {error}</p>}
+            <div className='card-actions justify-center mt-3 flex flex-row'>
+              <button className='btn btn-success grow' onClick={signUpHandler}>
+                SignUp
+              </button>
+              <button className='btn btn-neutral  grow' onClick={signInHandler}>
+                SignIn
+              </button>
             </div>
-            <input
-              type='text'
-              placeholder='Enter First Name'
-              value={firstName}
-              className='input input-bordered w-full max-w-xs'
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </label>
-          <label className='form-control w-full max-w-xs'>
-            <div className='label'>
-              <span className='label-text'>Last Name</span>
-            </div>
-            <input
-              type='text'
-              placeholder='Enter Last Name'
-              value={lastName}
-              className='input input-bordered w-full max-w-xs'
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </label>
-          <label className='form-control w-full max-w-xs'>
-            <div className='label'>
-              <span className='label-text'>Email</span>
-            </div>
-            <input
-              type='text'
-              placeholder='Enter Email'
-              value={email}
-              className='input input-bordered w-full max-w-xs'
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-          <label className='form-control w-full max-w-xs'>
-            <div className='label'>
-              <span className='label-text'>Password</span>
-            </div>
-            <input
-              type='password'
-              placeholder='Enter Password'
-              value={password}
-              className='input input-bordered w-full max-w-xs'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <div className='card-actions justify-center mt-3 flex flex-row'>
-            <button className='btn btn-success grow' onClick={signUpHandler}>
-              SignUp
-            </button>
-            <button className='btn btn-neutral  grow' onClick={signInHandler}>
-              SignIn
-            </button>
           </div>
         </div>
       </div>
-    </div>
+      <div className='toast toast-top toast-center'>
+        {showErrorToast && (
+          <div className='alert alert-error'>
+            <span>Oops, Account Creation Failed</span>
+          </div>
+        )}
+
+        {showSuccessToast && (
+          <div className='alert alert-success'>
+            <span>Account Created Successfully!</span>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 export default SignUp;
