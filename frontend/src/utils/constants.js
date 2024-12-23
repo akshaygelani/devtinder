2ee46ebcd09de2addb44d44c5a95e7e96c398e49
akshaygelani <akshaygelani786@gmail.com>
2024-12-23 11:17:23 +0530

    feat: pickup server port from env

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'http://localhost:3000';
+import { config } from 'dotenv';
+config();
+export const env = process.env;
+export const DEV_SERVER_PORT = env.DEV_SERVER_PORT;
+export const API_BASE_URL = env.API_BASE_URL;
