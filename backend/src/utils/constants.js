c8048b3c729abad1072a3529a6df4cc59bc40b5e
akshaygelani <akshaygelani786@gmail.com>
2024-12-24 13:48:22 +0530

    feat: trying solution to get actual client ip

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -8,11 +8,16 @@ export const IS_PROD = process.env.NODE_ENV === 'production';
 export const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
 const url = new URL(FRONTEND_BASE_URL);
 export const FRONTEND_HOSTNAME = url.hostname;
+
+function getRealIp(req) {
+  return req.headers['cf-connecting-ip'] || req.ip;
+}
 export const RATE_LIMIT_CONFIG = {
   windowMs: env.WINDOW_MS,
   limit: env.LIMIT,
   handler: (req, res) => {
-    console.log(`Rate limit exceeded for IP: ${req.ip}`);
+    const clientIp = getRealIp(req);
+    console.log(`Rate limit exceeded for IP: ${clientIp}`);
     return res.status(429).json({
       error: 'Too many requests, please try again later.',
     });
