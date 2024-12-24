96056830b29fae1d589461809180f11f4736aa61
akshaygelani <akshaygelani786@gmail.com>
2024-12-24 13:21:37 +0530

    feat: log the rate limiter ip

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -8,4 +8,10 @@ export const IS_PROD = process.env.NODE_ENV === 'production';
 export const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
 const url = new URL(FRONTEND_BASE_URL);
 export const FRONTEND_HOSTNAME = url.hostname;
-export const RATE_LIMIT_CONFIG = { windowMs: env.WINDOW_MS, limit: env.LIMIT };
+export const RATE_LIMIT_CONFIG = {
+  windowMs: env.WINDOW_MS,
+  limit: env.LIMIT,
+  onLimitReached: (req, res, options) => {
+    console.log(`Rate limit exceeded for IP: ${req.ip}`);
+  },
+};
